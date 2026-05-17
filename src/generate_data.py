559d175098f31f90f7f58544355a3a00fe0c8d02
Itqon el fikry fausta (083) <itqon0ft.2025@student.uny.ac.id>
2026-05-18 03:27:42 +0700

    Update(generate_data.py)
    : tambah looping untuk fungsi generate_data

--- src/generate_data.py
+++ src/generate_data.py
@@ -1,3 +1,9 @@
 def generate_data(n=80):
     kata = ['Algoritma','Jaringan','Python','Data','Digital',
-            'Sistem','Kontrol','Sinyal','Elektronika','Fisika']    
+            'Sistem','Kontrol','Sinyal','Elektronika','Fisika']
+    
+    return [Buku(f'ISBN-{i:04d}',
+                f'{random.choice(kata)} Vol.{i}',
+                f'Penulis-{random.randint(1,20)}',
+                random.choice(KATEGORI))
+            for i in range(1, n+1)]
