7c17ffb5bbbb1ca4d113008f49d438f31b5d4fcb
Itqon el fikry fausta (083) <itqon0ft.2025@student.uny.ac.id>
2026-05-18 02:47:05 +0700

    Update(graph.py)
    : tambah fungsi add_pinjam

--- src/data_structures/graph.py
+++ src/data_structures/graph.py
@@ -1,3 +1,13 @@
 class GraphRekBuku:
     def _init_(self):
         self.adj = {}
+        
+    def add_copinjam(self, a, b):
+        if a == b:
+            return
+
+        self.adj.setdefault(a, {})
+        self.adj.setdefault(b, {})
+
+        self.adj[a][b] = self.adj[a].get(b, 0) + 1
+        self.adj[b][a] = self.adj[b].get(a, 0) + 1
