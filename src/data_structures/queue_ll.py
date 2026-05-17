de389f56a97b25115a320edd8fbb48ad30f19231
Itqon el fikry fausta (083) <itqon0ft.2025@student.uny.ac.id>
2026-05-18 01:47:09 +0700

    Update(queue):
     tambah enqueue dan dequeque

--- src/data_structures/queue_ll.py
+++ src/data_structures/queue_ll.py
@@ -4,7 +4,30 @@ class LLNode:
         self.next = None
         
 class Queue:
+    
     def _init_(self):
         self.head = None
         self.tail = None
         self._size = 0
+        
+    def enqueue(self, data):
+        node = LLNode(data)
+        if not self.tail:
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+        self._size += 1
+
+    def dequeue(self):
+        if not self.head:
+            return None
+        data = self.head.data
+        self.head = self.head.next
+        if not self.head:
+            self.tail = None
+        self._size -= 1
+        return data
+
+def _len_(self):
+        return self._size
