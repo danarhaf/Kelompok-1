b6edbde42cb85c3e037b5f8c28fc94cc64e2160a
Itqon el fikry fausta (083) <itqon0ft.2025@student.uny.ac.id>
2026-05-18 00:03:11 +0700

    Update(data_model.py)
    : tambah dataclass seusai panduan starter code

--- src/data_model.py
+++ src/data_model.py
@@ -9,3 +9,19 @@ random.seed(13)
 
 KATEGORI = ['Fiksi', 'Sains', 'Teknik', 'Sejarah', 'Seni']
 STATUS   = {'TERSEDIA': 0, 'DIPINJAM': 1, 'DIPESAN': 2}
+
+@dataclass
+class Buku:
+    isbn      : str   
+    judul     : str
+    pengarang : str
+    kategori  : str
+    status    : int = 0   
+
+@dataclass
+class Peminjaman:
+    transaksi_id : int
+    anggota_id   : str          
+    isbn         : str
+    tgl_pinjam   : float = field(default_factory=time.time)  
+    durasi_hari  : int   = 14   
