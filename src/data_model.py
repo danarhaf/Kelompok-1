3185471772c008f1cf28143aa049250d875d2b1a
Itqon el fikry fausta (083) <itqon0ft.2025@student.uny.ac.id>
2026-05-17 23:56:19 +0700

    Update(data_model.py)
    : tambah library sesuai panduan starter code

---
+++ src/data_model.py
@@ -0,0 +1,5 @@
+import numpy as np          
+import time                 
+import random               
+from dataclasses import dataclass, field   
+from typing import Optional, List, Dict, Tuple  
