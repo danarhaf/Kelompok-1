e57f8a414fd2ef9b83094fd85d069fd24cc03486
Itqon el fikry fausta (083) <itqon0ft.2025@student.uny.ac.id>
2026-05-18 01:12:53 +0700

    Update(queue):
     tambah class queue

--- src/data_structures/queue_ll.py
+++ src/data_structures/queue_ll.py
@@ -2,3 +2,9 @@ class LLNode:
     def _init_(self, data=None):
         self.data = data
         self.next = None
+        
+class Queue:
+    def _init_(self):
+        self.head = None
+        self.tail = None
+        self._size = 0
